90383efc57972d02d95163e123f0092c5439d815
veereshgs <veeresh.gs@moreretail.in>
2023-04-03 22:20:58 +0530

    adde script

---
+++ sample.py
@@ -0,0 +1,30 @@
+
+class NSolution:
+
+    def ways_to_attend_classes(self, n):
+        if n<4:
+            return str(2**(n-1))+'/'+str(2**n)
+        A=2
+        AA=1
+        AAA=1
+        P = 4
+        count = 8
+        for i in range(4,n+1):
+            temp= AAA
+            AAA=AA
+            AA=A
+            A=P
+            P= count
+            count = (count-temp)*2+temp
+        return str(AAA+AA+A)+'/'+str(count)
+    
+
+
+r = NSolution()
+print(r.ways_to_attend_classes(5))
+
+
+r = NSolution()
+print(r.ways_to_attend_classes(10))
+
+
